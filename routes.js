e61e7210200243de89e45e73b099fb55a6dae895
Kim Drezdon <kdrezdon@gmail.com>
2019-10-06 13:04:40 -0500

    Add get courses routes

--- routes.js
+++ routes.js
@@ -29,34 +29,43 @@ function asyncHandler(cb) {
 //Send a GET request to /users to return the currently authenticated user (200)
 //needs to call custom authentication middleware first
 router.get('/users', asyncHandler( async (req, res) => {
-    // const user = req.currentUser;
 
-    // res.json({
-    //     name: user.name,
-    //     username: user.username
-    // })
-
-    const users = await User.findAll();
-    res.json(users);
-}))
+}));
 
 //Send a POST request to /users to create a user, set the Location header to '/' and return no content (201)
 //needs to validate that the request body contains these required values and return validation errors when necessary: firstName, lastName, emailAddress, password
 router.post('/users', asyncHandler( async (req, res) => {
 
-}))
+}));
 
 //Send a GET request to /courses to return a list of courses, including the user that owns each course (200)
+router.get('/courses', asyncHandler( async (req, res) => {
+    const courses = await Course.findAll();
+    res.status(200).json(courses);
+}));
 
 //Send a GET request to /courses/:id to return the course, including the user that owns the course, for the provided course ID (200)
+router.get('/courses/:id', asyncHandler( async (req, res) => {
+    const course = await Course.findByPk(req.params.id);
+    res.status(200).json(course);
+}));
 
 //Send a POST request to /courses to create a course, set the Location header to the URI for the course, and return no content (201)
 //needs to validate that the request body contains these required values and return validation errors when necessary: title, description
+router.post('/courses', asyncHandler( async (req, res) => {
+
+}));
 
 //Send a PUT request to /courses/:id to update a course and return no content (204)
 //needs to validate that the request body contains these required values and return validation errors when necessary: title, description
+router.put('/courses/:id', asyncHandler( async (req, res) => {
+
+}));
 
 //Send a DELETE request to /courses/:id to delete a course and return no content
+router.delete('/courses/:id', asyncHandler( async (req, res) => {
+
+}));
 
 //Export the router
 module.exports = router;
